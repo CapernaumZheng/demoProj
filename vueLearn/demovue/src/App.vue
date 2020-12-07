2b810cdf41daad09c002727e3a8b3da47983b5a2
zack zheng <zack_zhengxiyun@163.com>
2020-12-07 23:52:09 +0800

    vue demo

---
+++ vueLearn/demovue/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+      <p class="title">{{ title }}</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      title: 'vue cli项目'
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+#app {
+  .title {
+    color: blue;
+  }
+}
+</style>
